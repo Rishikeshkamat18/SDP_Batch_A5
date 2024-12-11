bfdadbb12b2ee9b052ce5c927169d19638e56aba
Rishikeshkamat18 <105357657+Rishikeshkamat18@users.noreply.github.com>
2024-12-11 13:40:28 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+pip install scikit-learn
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import streamlit as st
